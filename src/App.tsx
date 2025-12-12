33128a6824769d9e6231409f19274a8b65939bef
FreadSt <thiswolfram@gmail.com>
2025-12-12 19:46:21 +0200

    Added canvas for model

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import {Navbar} from "./components/Navbar.tsx";
 import React from "react";
 import Hero from "./components/Hero.tsx";
+import ProductViewer from "./components/ProductViewer.tsx";
 
 export const App: React.FC = () => {
   return (
     <>
      <Navbar/>
       <Hero/>
-      <h1 className='text-white'>Hello world</h1>
+      <ProductViewer/>
     </>
   )
 }
